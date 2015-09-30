b32a4e72cf42afa22d48835788fb2f1bf2343860
hungnguyen113278 <nguyenminhhung110387@gmail.com>
2015-09-30 10:00:55 +0700

    15
    15

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-javac *.java
-java run
-exit
+echo "Waiting for local test server to become available..."
+while [ $? -ne 0 ]; do
+    sleep 3
+    ./run.xml
+done
+
+echo "Waiting 10 seconds..."
+sleep 10
+
+echo "Running tests..."
+grunt selenium
