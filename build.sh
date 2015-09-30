e1689221c59ab8bec47aefb462ad3bc89a8a1cb0
hungnguyen113278 <nguyenminhhung110387@gmail.com>
2015-09-30 10:12:12 +0700

    19
    19

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
-echo "Waiting for local test server to become available..."
 while [ $? -ne 0 ]; do
     sleep 3
     ./run.xml
 done
-
-echo "Waiting 10 seconds..."
 sleep 10
-
-echo "Running tests..."
 grunt selenium
